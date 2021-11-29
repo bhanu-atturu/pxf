65e2e5f184152d0290322fdb49ad5f1b06c5aa58
Bradford D. Boyle <bradfordb@vmware.com>
2021-11-29 14:22:55 -0800

    fixup! WIP: Basic automation test for systemd restart

--- automation/src/test/java/org/greenplum/pxf/automation/smoke/SystemdSmoke.java
+++ automation/src/test/java/org/greenplum/pxf/automation/smoke/SystemdSmoke.java
@@ -1,7 +1,6 @@
 package org.greenplum.pxf.automation.smoke;
 
 import org.greenplum.pxf.automation.structures.tables.basic.Table;
-import org.greenplum.pxf.automation.structures.tables.pxf.ReadableExternalTable;
 import org.greenplum.pxf.automation.structures.tables.utils.TableFactory;
 import org.testng.annotations.Test;
 
@@ -14,16 +13,16 @@ public class SystemdSmoke extends BaseSmoke {
 
     @Override
     protected void createTables() throws Exception {
-        ReadableExternalTable externalTable = TableFactory.getPxfReadableTextTable("pxf_smoke_small_data", new String[]{
+        exTable = TableFactory.getPxfReadableTextTable("pxf_smoke_small_data", new String[] {
                 "name text",
                 "num integer",
                 "dub double precision",
                 "longNum bigint",
                 "bool boolean"
         }, hdfs.getWorkingDirectory() + "/" + fileName, ",");
-        externalTable.setHost(pxfHost);
-        externalTable.setPort(pxfPort);
-        gpdb.createTableAndVerify(externalTable);
+        exTable.setHost(pxfHost);
+        exTable.setPort(pxfPort);
+        gpdb.createTableAndVerify(exTable);
     }
 
     @Override
@@ -31,7 +30,7 @@ public class SystemdSmoke extends BaseSmoke {
         runTincTest("pxf.smoke.small_data.runTest");
     }
 
-    @Test(groups = "systemd")
+    @Test(groups = {"multiClusterSecurity", "systemd"})
     public void testRestart() throws Exception {
         prepareData();
         createTables();
@@ -43,15 +42,5 @@ public class SystemdSmoke extends BaseSmoke {
     private void shutdownPxf() throws Exception {
         cluster.runCommand("curl -X POST localhost:5888/actuator/shutdown");
         Thread.sleep(10_000L);
-
-//        int attempts = 10;
-//        do {
-//            attempts--;
-//            cluster.runCommand("curl localhost:5888/actuator/health");
-//            if (cluster.getLastCommandExitCode() == 0) {
-//                break;
-//            }
-//            Thread.sleep(1_000L);
-//        } while (attempts > 0);
     }
 }
